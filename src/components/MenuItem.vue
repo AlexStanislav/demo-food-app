daf53d4bf529b4c78a3fdfc6c3cf6001747e56bc
Alex Stanislav <stanislav.alexandru@gmail.com>
2025-01-22 13:40:21 +0200

    Applied BEM methodology

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,36 +1,38 @@
 <template>
-    <section class="menu-item">
-        <div class="img-container">
-            <img :src="`https://picsum.photos/600/600/?random=${item.menu_item_id}`" :alt="item.menu_item_name" />
+    <section class="item">
+        <div class="item__img-wrapper">
+            <img class="item__img" :src="`https://picsum.photos/600/600/?random=${item.menu_item_id}`"
+                :alt="item.menu_item_name" />
         </div>
-        <div class="img-loader">
+        <div class="item__img-loader">
             <ProgressSpinner stroke-width="4" />
         </div>
-        <div class="menu-item-info">
-            <h3 class="menu-item-name">
+        <div class="item__info">
+            <h3 class="item__name">
                 {{ item.menu_item_name.split('')[0].toUpperCase() + item.menu_item_name.slice(1) }}
                 <font-awesome-icon v-if="item.vegetarian" icon="fa-solid fa-leaf" color="var(--p-green-500)" size="lg"
                     style="margin-top: 0.25rem;" />
             </h3>
-            <div class="spice-rating">
+            <div class="item__spice-rating">
                 <font-awesome-icon v-for="i in 3" :key="i" icon="fa-solid fa-pepper-hot"
                     :color="i <= getSpiceRating(item) ? 'var(--p-amber-700)' : 'var(--p-amber-400)'"
                     style="font-size: 0.8rem;" />
             </div>
-            <div class="ingredients">
-                <h4>Ingredients</h4>
-                <ul>
-                    <li v-for="(ingredient, index) in getIngredients(item)" :key="index">{{
-                        ingredient.split('')[0].toUpperCase() + ingredient.slice(1) }}{{ index <
+            <div class="item__ingredients">
+                <h4 class="item__ingredients-title">Ingredients</h4>
+                <ul class="item__ingredients-list">
+                    <li v-for="(ingredient, index) in getIngredients(item)" :key="index">
+                        {{
+                            ingredient.split('')[0].toUpperCase() + ingredient.slice(1) }}{{ index <
                             getIngredients(item).length - 1 ? ',&nbsp;' : '' }}</li>
                 </ul>
             </div>
-            <p class="calories">{{ item.calories }} kcal</p>
+            <p class="item__calories">{{ item.calories }} kcal</p>
             <InputGroup>
                 <InputGroupAddon addon="price">$</InputGroupAddon>
                 <InputText :value="item.price" readonly />
             </InputGroup>
-            <p class="menu-section">{{ item.menu_section }}</p>
+            <p class="item__section">{{ item.menu_section }}</p>
         </div>
     </section>
 </template>
@@ -70,7 +72,7 @@ function getIngredients(item: FoodItems): string[] {
 
 </script>
 <style scoped>
-.menu-item {
+.item {
     width: 400px;
     display: flex;
     flex-direction: column;
@@ -82,19 +84,19 @@ function getIngredients(item: FoodItems): string[] {
     position: relative;
 }
 
-.img-container {
+.item__img-wrapper {
     width: 350px;
     height: 350px;
     position: relative;
 }
 
-img {
+.item__img {
     width: 100%;
     height: auto;
     border-radius: 0.5rem;
 }
 
-.img-loader {
+.item__img-loader {
     position: absolute;
     top: 0;
     left: 0;
@@ -108,7 +110,7 @@ img {
 
 
 
-.menu-item-info {
+.item__info {
     width: 100%;
     display: flex;
     flex-direction: column;
@@ -116,7 +118,7 @@ img {
     position: relative;
 }
 
-.menu-item-name {
+.item__name {
     margin: 0.5rem 0 0 0;
     font-size: 1.5rem;
     font-weight: 900;
@@ -130,23 +132,23 @@ img {
     font-size: 0.9rem;
 }
 
-.spice-rating {
+.item__spice-rating {
     display: flex;
     align-items: center;
     gap: 0.5rem;
 }
 
-.ingredients {
+.item__ingredients {
     margin-top: 0.75rem;
 }
 
-.ingredients h4 {
+.item__ingredients-title {
     margin: 0.5rem 0 0 0;
     font-size: 1.1rem;
     color: var(--p-amber-700);
 }
 
-.ingredients ul {
+.item__ingredients-list {
     margin: 0.5rem 0 0 0;
     padding: 0;
     list-style: none;
@@ -157,7 +159,7 @@ img {
     font-style: italic;
 }
 
-.calories {
+.item__calories {
     margin: 0.5rem 0 0 0;
     color: var(--p-amber-600);
     font-style: italic;
@@ -167,7 +169,7 @@ img {
     top: 0.5rem;
 }
 
-.menu-section {
+.item__section {
     margin: 0.5rem 0 0 0;
     color: var(--p-amber-600);
     font-weight: bold;
@@ -187,9 +189,5 @@ img {
 .p-inputgroupaddon {
     background: var(--p-amber-500);
     color: #fff;
-}
-
-.more-details {
-    background: var(--p-amber-600);
 }
 </style>
